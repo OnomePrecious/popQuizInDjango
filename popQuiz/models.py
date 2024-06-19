38f02a09fcf39d655573faab1516e99670cfbef7
Precious <preciousonome002@gmail.com>
2024-06-19 07:56:54 +0100

    popQuiz

--- popQuiz/models.py
+++ popQuiz/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+# create your models here.
+#
+#
+#
+
+
+class User(models.Model):
+    user_id = models.IntegerField()
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=100)
+
+
+class Admin(models.Model):
+    admin_id = models.IntegerField()
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=100)
+
