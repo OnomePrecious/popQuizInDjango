2635a1a13bb0d06ccb36aec44ffcec9028ba1bd2
Precious <preciousonome002@gmail.com>
2024-06-19 08:07:46 +0100

    popQuiz

--- quizDeliverySystem/urls.py
+++ quizDeliverySystem/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+admin.site.site_header = 'Pop quiz delivery system'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('popQuiz.urls')),
 ]
+admin.site.site_header = 'Account Management System'
+
+
