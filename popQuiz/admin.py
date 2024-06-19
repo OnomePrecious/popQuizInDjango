cfd5d0da9d564f3fa7175e5e6d76ff7ea3729b69
Precious <preciousonome002@gmail.com>
2024-06-19 08:01:24 +0100

    popQuiz

--- popQuiz/admin.py
+++ popQuiz/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+import popQuiz
+
+
 # Register your models here.
+@admin.register(popQuiz)
+class Admin(admin.ModelAdmin):
+    list_display = ['admin_id', 'first_name', 'last_name', 'password', 'email']
+    list_per_page = 10
+    search_fields = ['account_number', 'first_name', 'last_name']
+    list_editable = ['first_name', 'last_name', 'account_type']
--- popQuiz/models.py
+++ popQuiz/models.py
@@ -14,9 +14,3 @@ class User(models.Model):
     email = models.CharField(max_length=100)
 
 
-class Admin(models.Model):
-    admin_id = models.IntegerField()
-    username = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    email = models.CharField(max_length=100)
-
