10e80b1146915fcb16440afaf74d69adac4d91d3
Precious <preciousonome002@gmail.com>
2024-07-05 15:22:40 +0100

    popQuiz

--- popQuiz/views.py
+++ popQuiz/views.py
@@ -17,33 +17,42 @@ class QuizQuestions(APIView):
         serializer = QuizSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         title = serializer.data['title']
-        quiz = serializer.data['quiz']
+        description = serializer.data['description']
+        author = serializer.data['author']
         question = get_object_or_404(Quiz)
         question = generate_question(question)
+        Question.objects.create(
+            title=title, description=description,
+            author=author,
+            question_text=question
+
+        )
         return Response(question, status=status.HTTP_200_OK)
 
-@api_view()
-def quiz_questions():
-    quizzes = Quiz.objects.all()
-    serializer = QuizSerializer(quizzes, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
 
+# @api_view()
+# def quiz_questions():
+#     quizzes = Quiz.objects.all()
+#     serializer = QuizSerializer(quizzes, many=True)
+#     return Response(serializer.data, status=status.HTTP_200_OK)
+#
 
 @api_view()
-def list_of_questions():
-    questions = Question.objects.all()
+def list_of_questions(request):
+    user = request.user
+    questions = get_object_or_404(Question, user=user.id)
     serializer = QuestionSerializer(questions, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@staff_member_required
-def create_quiz(request):
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        description = request.POST.get('description')
-        author = request.admin
-        quiz = Quiz.objects.create(title=title, description=description, author=author)
-        serializer = QuizSerializer(quiz, many=True)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
+#
+#
+# @staff_member_required
+# def create_quiz(request):
+#     if request.method == 'POST':
+#         title = request.POST.get('title')
+#         description = request.POST.get('description')
+#         author = request.admin
+#         quiz = Quiz.objects.create(title=title, description=description, author=author)
+#         serializer = QuizSerializer(quiz, many=True)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#
+#
